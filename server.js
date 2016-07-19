69084127c8fc23c22c6e4dd4a551b57c57f5f847
Robby Kim <robert.st.kim@gmail.com>
2016-07-18 23:11:17 -0400

    added parallel api requests for top tracks of related artists

--- server.js
+++ server.js
@@ -1,48 +1,67 @@
-var unirest = require( 'unirest' );
-var express = require( 'express' );
-var events = require( 'events' );
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
 
-var getFromApi = function( endpoint, args ) {
+var getFromApi = function (endpoint, args) {
   var emitter = new events.EventEmitter();
-  unirest.get( 'https://api.spotify.com/v1/' + endpoint )
-    .qs( args )
-    .end( function( response ) {
-      if ( response.ok ) {
-        emitter.emit( 'end', response.body );
+  unirest.get('https://api.spotify.com/v1/' + endpoint)
+    .qs(args)
+    .end(function (response) {
+      if (response.ok) {
+        emitter.emit('end', response.body);
       } else {
-        emitter.emit( 'error', response.code );
+        emitter.emit('error', response.code);
       }
-    } );
-  console.log( emitter, '<-----EMITTER' );
+    });
+  console.log(emitter, '<-----EMITTER');
   return emitter;
 };
 
 var app = express();
-app.use( express.static( 'public' ) );
+app.use(express.static('public'));
 
-app.get( '/search/:name', function( req, res ) {
-  var searchReq = getFromApi( 'search', {
+app.get('/search/:name', function (req, res) {
+  var searchReq = getFromApi('search', {
     q: req.params.name,
     limit: 1,
     type: 'artist'
-  } );
+  });
 
-  searchReq.on( 'end', function( item ) {
-    var artist = item.artists.items[ 0 ];
-    var relatedReq = getFromApi( 'artists/' + artist.id + '/related-artists' );
-    relatedReq.on( 'end', function( item ) {
+  searchReq.on('end', function (item) {
+    var artist = item.artists.items[0];
+    var relatedReq = getFromApi('artists/' + artist.id + '/related-artists');
+    relatedReq.on('end', function (item) {
       artist.related = item.artists;
-      res.json( artist );
-    } );
 
-    relatedReq.on( 'error', function( code ) {
-      res.sendStatus( 404 );
-    } );
-  } );
+      var completed = 0;
+      var checkComplete = function () {
+        if (completed === artist.related.length) {
+          res.json(artist);
+        }
+      };
 
-  searchReq.on( 'error', function( code ) {
-    res.sendStatus( code );
-  } );
-} );
+      artist.related.forEach(function (value, index) {
+        var topTracks = getFromApi('artists/' + artist.related[index].id + '/top-tracks');
+        topTracks.on('end', function (item) {
+          artist.related[index].tracks = item.tracks;
+          completed += 1;
+          checkComplete();
+        });
 
-app.listen( 8080 );
+        topTracks.on('error', function (code) {
+          res.sendStatus(code);
+        });
+      });
+    });
+
+    relatedReq.on('error', function (code) {
+      res.sendStatus(code);
+    });
+  });
+
+  searchReq.on('error', function (code) {
+    res.sendStatus(code);
+  });
+});
+
+app.listen(8080);
